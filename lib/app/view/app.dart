f682f158e8eb92ef14cf403a30be184198f67f61
teblesz <mteblesz@gmail.com>
2024-01-12 02:36:50 +0100

    Update flutter version (add null checks)

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -6,6 +6,7 @@ import 'package:fluttartur/app/app.dart';
 import 'package:fluttartur/theme.dart';
 import 'package:data_repository/data_repository.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 
 // MAIN TODO's:
