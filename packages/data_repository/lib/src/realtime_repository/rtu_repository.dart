deecc0a07f0091e228056f5663ab4c8c4856cedc
teblesz <mteblesz@gmail.com>
2024-01-28 04:41:41 +0100

    Update to forked package

--- packages/data_repository/lib/src/realtime_repository/rtu_repository.dart
+++ packages/data_repository/lib/src/realtime_repository/rtu_repository.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'package:data_repository/src/data_cache.dart';
 import 'package:data_repository/src/realtime_repository/rtu_config.dart';
-import 'package:signalr_netcore/signalr_client.dart';
+import 'package:signalr_client/signalr_client.dart';
 import '../../models/models.dart';
 
 class RtuRepository {
@@ -14,14 +14,18 @@ class RtuRepository {
   late HubConnection hubConnection;
   late StreamController<List<PlayerInfoDto>> _playerStreamController;
 
-  void connect() {
-    hubConnection.start();
+  Future<void> connect() async {
+    try {
+      await hubConnection.start();
+    } on Exception catch (e) {
+      print(e);
+    }
   }
 
   void listenPlayers() {
     _playerStreamController = StreamController<List<PlayerInfoDto>>.broadcast();
     hubConnection.on("ReceivePlayerList", (arguments) {
-      final updatedPlayerList = List<PlayerInfoDto>.from(arguments![0]);
+      final updatedPlayerList = List<PlayerInfoDto>.from(arguments![0] as List);
       _playerStreamController.add(updatedPlayerList);
     });
   }
