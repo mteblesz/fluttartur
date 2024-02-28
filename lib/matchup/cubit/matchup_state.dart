ced50a107552a73f76a2882025c350524caebb4c
teblesz <mteblesz@gmail.com>
2024-02-28 23:47:27 +0100

    Add roles state properties

--- lib/matchup/cubit/matchup_state.dart
+++ lib/matchup/cubit/matchup_state.dart
@@ -8,17 +8,30 @@ class MatchupState extends Equatable {
     this.errorMessage,
     this.playersCount = 0,
     this.isHost = false,
+    this.areMerlinAndAssassinInGame = false,
+    this.arePercivalAreMorganaInGame = false,
+    this.areOberonAndMordredInGame = false,
   });
 
   final Nick nick;
-  // TODO add chharacter config status here
   final FormzStatus status;
   final String? errorMessage;
   final int playersCount;
   final bool isHost;
+  final bool areMerlinAndAssassinInGame;
+  final bool arePercivalAreMorganaInGame;
+  final bool areOberonAndMordredInGame;
 
   @override
-  List<Object> get props => [nick, status, playersCount, isHost];
+  List<Object> get props => [
+        nick,
+        status,
+        playersCount,
+        isHost,
+        areMerlinAndAssassinInGame,
+        arePercivalAreMorganaInGame,
+        areOberonAndMordredInGame,
+      ];
 
   MatchupState copyWith({
     Nick? nick,
@@ -26,6 +39,9 @@ class MatchupState extends Equatable {
     String? errorMessage,
     int? playersCount,
     bool? isHost,
+    bool? areMerlinAndAssassinInGame,
+    bool? arePercivalAreMorganaInGame,
+    bool? areOberonAndMordredInGame,
   }) {
     return MatchupState(
       nick: nick ?? this.nick,
@@ -33,6 +49,12 @@ class MatchupState extends Equatable {
       errorMessage: errorMessage ?? this.errorMessage,
       playersCount: playersCount ?? this.playersCount,
       isHost: isHost ?? this.isHost,
+      areMerlinAndAssassinInGame:
+          areMerlinAndAssassinInGame ?? this.areMerlinAndAssassinInGame,
+      arePercivalAreMorganaInGame:
+          arePercivalAreMorganaInGame ?? this.arePercivalAreMorganaInGame,
+      areOberonAndMordredInGame:
+          areOberonAndMordredInGame ?? this.areOberonAndMordredInGame,
     );
   }
 }
