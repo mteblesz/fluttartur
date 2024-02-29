b95a67ee07bbd98fc82c5076c3e81d15dadac967
teblesz <mteblesz@gmail.com>
2024-02-29 22:27:58 +0100

    Add OutlinedText class to project

---
+++ lib/widgets/outlined_text.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class OutlinedText extends Text {
+  OutlinedText(
+    super.data, {
+    super.key,
+    super.style,
+    super.strutStyle,
+    super.textAlign,
+    super.textDirection,
+    super.locale,
+    super.softWrap,
+    super.overflow,
+    super.textScaleFactor,
+    super.textScaler,
+    super.maxLines,
+    super.semanticsLabel,
+    super.textWidthBasis,
+    super.textHeightBehavior,
+    super.selectionColor,
+    Color outlineColor = const Color.fromARGB(255, 100, 100, 100),
+    double outlineWidth = 2,
+  }) : outlineStyle = TextStyle(
+          foreground: Paint()
+            ..style = PaintingStyle.stroke
+            ..strokeWidth = outlineWidth
+            ..color = outlineColor,
+        ).merge(style);
+
+  final TextStyle outlineStyle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        copyAsTextWith(style: outlineStyle), // outline
+        super.build(context), // build as Text
+      ],
+    );
+  }
+
+  Text copyAsTextWith({
+    String? data,
+    Key? key,
+    TextStyle? style,
+    StrutStyle? strutStyle,
+    TextAlign? textAlign,
+    TextDirection? textDirection,
+    Locale? locale,
+    bool? softWrap,
+    TextOverflow? overflow,
+    double? textScaleFactor,
+    TextScaler? textScaler,
+    int? maxLines,
+    String? semanticsLabel,
+    TextWidthBasis? textWidthBasis,
+    TextHeightBehavior? textHeightBehavior,
+    Color? selectionColor,
+  }) {
+    return Text(
+      data ?? this.data!,
+      key: key ?? this.key,
+      style: style ?? this.style,
+      strutStyle: strutStyle ?? this.strutStyle,
+      textAlign: textAlign ?? this.textAlign,
+      textDirection: textDirection ?? this.textDirection,
+      locale: locale ?? this.locale,
+      softWrap: softWrap ?? this.softWrap,
+      overflow: overflow ?? this.overflow,
+      textScaleFactor: textScaleFactor ?? this.textScaleFactor,
+      textScaler: textScaler ?? this.textScaler,
+      maxLines: maxLines ?? this.maxLines,
+      semanticsLabel: semanticsLabel ?? this.semanticsLabel,
+      textWidthBasis: textWidthBasis ?? this.textWidthBasis,
+      textHeightBehavior: textHeightBehavior ?? this.textHeightBehavior,
+      selectionColor: selectionColor ?? this.selectionColor,
+    );
+  }
+}
