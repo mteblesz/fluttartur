f2be195885f29f69a62db641990bb2d501840209
teblesz <mteblesz@gmail.com>
2024-03-06 23:55:56 +0100

    Remove compiling errors

--- lib/game/view/game_page.dart
+++ lib/game/view/game_page.dart
@@ -1,5 +1,5 @@
 import 'package:fluttartur/game/cubit/game_cubit.dart';
-//import 'package:fluttartur/game/view/game_form.dart';
+import 'package:fluttartur/game/view/game_form.dart';
 import 'package:fluttartur/home/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -44,7 +44,7 @@ class GamePage extends StatelessWidget {
           ),
           body: BlocProvider(
             create: (_) => GameCubit(context.read<IDataRepository>()),
-            //child: const GameForm(), //TODO uncomment
+            child: const GameForm(),
           ),
         ),
       ],
