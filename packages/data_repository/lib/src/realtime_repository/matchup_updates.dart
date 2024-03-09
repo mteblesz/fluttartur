2631138aee0be01427810e85ed9506b61ab73f2e
teblesz <mteblesz@gmail.com>
2024-03-10 00:52:16 +0100

    Move rtu methods to extension

---
+++ packages/data_repository/lib/src/realtime_repository/matchup_updates.dart
@@ -0,0 +1,43 @@
+part of 'rtu_repository.dart';
+
+extension SquadsInfoUpdates on RtuRepository {
+  void subscribePlayersList() {
+    _playerStreamController = StreamController<List<Player>>.broadcast();
+    hubConnection.on("ReceivePlayerList", (List<Object?>? args) {
+      if (args != null && args.isNotEmpty && args[0] is List) {
+        final data = args[0] as List<dynamic>;
+        final dtos = data.map((data) => PlayerInfoDto.fromJson(data));
+        final updatedPlayers = dtos.map((e) => e.toPLayer()).toList();
+        _playerStreamController.add(updatedPlayers);
+      }
+    });
+  }
+
+  void unsubscribePlayersList() {
+    hubConnection.off("ReceivePlayerList");
+    _playerStreamController.close();
+  }
+
+  void handlePlayerRemoval({
+    required int playerId,
+    required void Function() removalHandler,
+  }) {
+    hubConnection.on("ReceiveRemoval", (List<Object?>? args) {
+      if (args != null && args.isNotEmpty) {
+        final removedplayerId = args[0] as String;
+        if (removedplayerId == playerId.toString()) {
+          hubConnection.off("ReceiveRemoval");
+          removalHandler();
+          dispose();
+        }
+      }
+    });
+  }
+
+  void handleGameStarted({required void Function() startGameHandler}) {
+    hubConnection.on("ReceiveStartGame", (List<Object?>? args) {
+      hubConnection.off("ReceiveStartGame");
+      startGameHandler();
+    });
+  }
+}
--- packages/data_repository/lib/src/realtime_repository/rtu_repository.dart
+++ packages/data_repository/lib/src/realtime_repository/rtu_repository.dart
@@ -4,6 +4,9 @@ import '../dtos/dtos.dart';
 import '../../model/model.dart';
 import 'package:signalr_netcore/signalr_client.dart';
 
+part 'squads_info_updates.dart';
+part 'matchup_updates.dart';
+
 class RtuRepository {
   RtuRepository() {
     final serverUrl = RtuConfig.url;
@@ -29,44 +32,4 @@ class RtuRepository {
 
   late StreamController<List<Player>> _playerStreamController;
   Stream<List<Player>> get playerStream => _playerStreamController.stream;
-
-  void subscribePlayersList() {
-    _playerStreamController = StreamController<List<Player>>.broadcast();
-    hubConnection.on("ReceivePlayerList", (List<Object?>? args) {
-      if (args != null && args.isNotEmpty && args[0] is List) {
-        final data = args[0] as List<dynamic>;
-        final dtos = data.map((data) => PlayerInfoDto.fromJson(data));
-        final updatedPlayers = dtos.map((e) => e.toPLayer()).toList();
-        _playerStreamController.add(updatedPlayers);
-      }
-    });
-  }
-
-  void unsubscribePlayersList() {
-    hubConnection.off("ReceivePlayerList");
-    _playerStreamController.close();
-  }
-
-  void handlePlayerRemoval({
-    required int playerId,
-    required void Function() removalHandler,
-  }) {
-    hubConnection.on("ReceiveRemoval", (List<Object?>? args) {
-      if (args != null && args.isNotEmpty) {
-        final removedplayerId = args[0] as String;
-        if (removedplayerId == playerId.toString()) {
-          hubConnection.off("ReceiveRemoval");
-          removalHandler();
-          dispose();
-        }
-      }
-    });
-  }
-
-  void handleGameStarted({required void Function() startGameHandler}) {
-    hubConnection.on("ReceiveStartGame", (List<Object?>? args) {
-      hubConnection.off("ReceiveStartGame");
-      startGameHandler();
-    });
-  }
 }
