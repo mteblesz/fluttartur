5414f70d5768c5d9a19710d6b0b987daa9a6ba62
mteblesz <mteblesz@gmail.com>
2024-04-28 01:46:27 +0200

    Change TeamWrap widget

--- lib/game/view/team_wrap.dart
+++ lib/game/view/team_wrap.dart
@@ -28,8 +28,7 @@ class _TeamWrap extends StatelessWidget {
                     style: const TextStyle(fontSize: 30)),
                 Expanded(
                   child: SingleChildScrollView(
-                    child: //_SquadListView(), //TODO uncomment
-                        const Text("<members list>"), //TODO remove
+                    child: _SquadListView(),
                   ),
                 ),
               ],
@@ -44,45 +43,51 @@ class _TeamWrap extends StatelessWidget {
 class _PlayerListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<List<Player>>(
-      stream: context.read<IDataRepository>().streamPlayersList(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const CircularProgressIndicator();
-        }
-        if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
-        }
-        List<Player> players = snapshot.data ?? List.empty();
-        return players.isEmpty
-            ? const Text("<court is empty>")
-            : Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  ...players.map(
-                    (player) => _PlayerCard(
-                        player: player,
-                        isLeader: player.playerId == "" //currentSquad.leaderId,
-                        // TODO change bloc above
-                        ),
-                  ),
-                ],
-              );
-      },
-    );
+    return BlocBuilder<CourtCubit, CourtState>(builder: (context, state) {
+      return state.courtiers.isEmpty
+          ? const Text('<court is empty>\nthis should not happen')
+          : Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                ...state.courtiers.map(
+                  (courtier) => _CourtierCard(courtier: courtier),
+                ),
+              ],
+            );
+    });
   }
 }
 
-class _PlayerCard extends StatelessWidget {
-  const _PlayerCard({required this.player, required this.isLeader});
+class _SquadListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<CourtCubit, CourtState>(builder: (context, state) {
+      return state.courtiers.isEmpty
+          ? const Text('<squad is empty>') // TODO Add this text to Localization
+          : Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                ...state.courtiers.where((courtier) => courtier.isMember).map(
+                      (courtier) => _CourtierCard(courtier: courtier),
+                    ),
+              ],
+            );
+    });
+  }
+}
 
-  final Player player;
-  final bool isLeader;
+class _CourtierCard extends StatelessWidget {
+  const _CourtierCard({
+    required this.courtier,
+  });
+
+  final Courtier courtier;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => context.read<GameCubit>().addMember(player: player),
+      onTap: () =>
+          context.read<CourtCubit>().removeMember(playerId: courtier.playerId),
       child: Card(
         margin: const EdgeInsets.all(1.0),
         child: Padding(
@@ -90,86 +95,20 @@ class _PlayerCard extends StatelessWidget {
           child: Row(
             mainAxisSize: MainAxisSize.min,
             children: [
-              isLeader ? const Icon(Icons.star) : const SizedBox.shrink(),
+              courtier.isLeader
+                  ? const Icon(Icons.star)
+                  : const SizedBox.shrink(),
               Flexible(
                 child: Text(
-                  player.nick,
+                  courtier.nick,
                   style: TextStyle(
                     fontSize: 23,
-                    fontWeight: player.playerId == 0 //TODO uncomment
-                        //context.read<IDataRepository>().currentPlayer.id
-                        ? FontWeight.bold
-                        : null,
+                    fontWeight:
+                        courtier.isCurrentPlayer ? FontWeight.bold : null,
                   ),
                 ),
               ),
             ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class _SquadListView extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO move curretnsqadid to cubit and make blocbuilder here instead
-    return //StreamBuilder<String>(
-        //   stream: context.read<IDataRepository>().streamCurrentSquadId(),
-        //   builder: (context, snapshot) {
-        //     var currentSquadId = snapshot.data;
-        //     return currentSquadId == null
-        //         ? const Text('<squad is empty>')
-        //         :
-        StreamBuilder<List<Player>>(
-      stream: context.read<GameCubit>().streamMembersList(),
-      builder: (context, snapshot) {
-        var members = snapshot.data;
-        return members == null
-            ? const Text('<squad is empty>')
-            : Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  ...members.map(
-                    (member) => _MemberCard(member: member),
-                  ),
-                ],
-              );
-      },
-    );
-    //  },
-    // );
-  }
-}
-
-class _MemberCard extends StatelessWidget {
-  // TODO ! make it a hero widget between two lists
-  const _MemberCard({
-    required this.member,
-  });
-
-  final Player member;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => context.read<GameCubit>().removeMember(member: member),
-      child: Card(
-        margin: const EdgeInsets.all(1.0),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Flexible(
-            child: Text(
-              member.nick,
-              style: TextStyle(
-                fontSize: 23,
-                fontWeight: member.playerId == 1 //TODO uncomment
-                    // context.read<IDataRepository>().currentPlayer.id
-                    ? FontWeight.bold
-                    : null,
-              ),
-            ),
           ),
         ),
       ),
